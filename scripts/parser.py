79aeaeb332620cac37e8b98567bd68c0bda52965
fyrva <karkatinel@gmail.com>
2025-03-08 00:11:18 +0300

    добавил свой парсер

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,15 +1,28 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.expected_conditions import all_of
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.webdriver.support.expected_conditions import text_to_be_present_in_element
 
-driver = webdriver.Edge()
+
+keys = ['publication number',
+        'name',
+        'date of publication',
+        'ipc',
+        'application number',
+        'applicant',
+        'inventor',
+        'patent']
+
+driver = webdriver.Chrome()
 driver.get(r"https://patentscope.wipo.int/search/ru/advancedSearch.jsf")
-time.sleep(9)
 
+WebDriverWait(driver, 20).until(presence_of_element_located((By.CLASS_NAME, "ps-office--input")))
 driver.find_element(By.CLASS_NAME, "ps-office--input").click()
-time.sleep(2)
+
 for opt in driver.find_elements(By.CLASS_NAME, "ps-office--options--item"):
     if opt.text == "Франция":
         opt.find_element(By.TAG_NAME, "input").click()
@@ -21,56 +34,48 @@ select_lang.select_by_value("fr")
 search = driver.find_element(By.ID, "advancedSearchForm:searchButton")
 search.click()
 
-time.sleep(10)
-
-#select_num_elements = Select(driver.find_element(By.ID, "resultListCommandsForm:perPage:input"))
-#select_num_elements.select_by_value("200")
-#time.sleep(10)
-
-dataset = []
-for i in range(3):
-    if i:
-        time.sleep(10)
-        next_page = driver.find_element(By.XPATH, r'//*[@id="resultListCommandsForm:j_idt2031"]')
-        next_page.click()
-        time.sleep(5)
-    c = 0
+c = 1
+WebDriverWait(driver, 20).until(presence_of_element_located((By.ID, "resultListCommandsForm:perPage:input")))
+select_num_elements = Select(driver.find_element(By.ID, "resultListCommandsForm:perPage:input"))
+select_num_elements.select_by_value("200")
+for i in range(20):
+    WebDriverWait(driver, 20).until(
+        presence_of_element_located((By.CSS_SELECTOR, '[id="resultListForm:resultTable:199:patentResult"]'))
+    )
     table = driver.find_element(By.ID, "resultListForm:resultTable_data")
+    results = []
     for cell in table.find_elements(By.TAG_NAME, "tr"):
-        data_per_cell = {"patent_number": 0,
-                        "name": "",
-                        "date": "",
-                        "application_number": 0,
-                        "applicant": "",
-                        "inventor": "",
-                        "text": ""}
-        patent_number = cell.find_element(By.XPATH, fr'//*[@id="resultListForm:resultTable:{c}:patentResult"]/div[1]/div[1]/a/span')
-        data_per_cell["patent_number"] = int(patent_number.text)
-
-        name = cell.find_element(By.XPATH, fr'//*[@id="resultListForm:resultTable:{c}:patentResult"]/div[1]/div[1]/span[2]/span')
-        data_per_cell["name"] = name.text
-
-        date = cell.find_element(By.XPATH, fr'//*[@id="resultListForm:resultTable:{c}:resultListTableColumnPubDate"]')
-        data_per_cell["date"] = date.text
-
-        application_number = cell.find_element(By.XPATH, fr'/html/body/div[2]/div[4]/div/div[1]/div[2]/div/form[2]/div/div[1]/div/div/table/tbody/tr[{c + 1}]/td/div/div[2]/div/div[1]/span[2]/span[2]')
-        data_per_cell["application_number"] = int(application_number.text)
-
-        applicant = cell.find_element(By.XPATH, fr'/html/body/div[2]/div[4]/div/div[1]/div[2]/div/form[2]/div/div[1]/div/div/table/tbody/tr[{c + 1}]/td/div/div[2]/div/div[1]/span[3]/span[2]')
-        data_per_cell["applicant"] = applicant.text
-
-        inventor = cell.find_element(By.XPATH, fr'/html/body/div[2]/div[4]/div/div[1]/div[2]/div/form[2]/div/div[1]/div/div/table/tbody/tr[{c + 1}]/td/div/div[2]/div/div[1]/span[4]/span[2]')
-        data_per_cell["inventor"] = inventor.text
-
-        text = cell.find_element(By.XPATH, fr'/html/body/div[2]/div[4]/div/div[1]/div[2]/div/form[2]/div/div[1]/div/div/table/tbody/tr[{c + 1}]/td/div/div[2]/div/div[2]/span')
-        data_per_cell["text"] = text.text
-
-        dataset.append(data_per_cell)
+        pubnum = cell.find_element(By.TAG_NAME, 'a').text
+        name = cell.find_element(By.CSS_SELECTOR, '[class="ps-patent-result--title--title content--text-wrap"]').text
+        date = cell.find_element(By.CSS_SELECTOR, '[class="ps-patent-result--title--ctr-pubdate"]').text
+        ipcspan = cell.find_element(By.CSS_SELECTOR, '[class="ps-field ps-field--is-layout--inline "]')
+        ipc = ipcspan.find_elements(By.TAG_NAME, 'span')[1].text
+        application = cell.find_element(By.CSS_SELECTOR, '[class="ps-field--value notranslate"]').text
+        applicant = cell.find_element(By.CSS_SELECTOR, '[class="ps-field--value ps-patent-result--applicant notranslate"]').text
+        inventor = cell.find_element(By.CSS_SELECTOR, '[class="ps-field--value ps-patent-result--inventor notranslate"]').text
+        patent = cell.find_element(By.CSS_SELECTOR, '[class="ui-outputpanel ui-widget ps-patent-result--abstract"]').text
+        results.append({
+            'publication number': pubnum,
+            'name': name,
+            'date of publication': date,
+            'ipc': ipc,
+            'application number': application,
+            'applicant': applicant,
+            'inventor': inventor,
+            'patent': patent
+        })
+    for t in results:
+        print(c)
+        for j in keys:
+            print(f'{j}:{t[j]}')
+        print('\n')
         c += 1
+    print(f"{i+1} завершено")
+    #сюда добавить импорт в csv, по 200 записей как раз норм будет
+    driver.find_element(By.CSS_SELECTOR, '[class="ui-commandlink ui-widget ps-link--has-icon js-paginator-next"]').click()
+    WebDriverWait(driver, 10).until(
+        text_to_be_present_in_element((By.CSS_SELECTOR, '[id="resultListForm:pageNumber"]'), f'{i+2}')
+    )
+    driver.refresh()
 
-print(dataset[0])
-print(dataset[-1])
-print(len(dataset))
-
-time.sleep(10)
 driver.close()
