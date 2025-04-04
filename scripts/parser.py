fe541bc1a9a179ad763580c6b334968a0fe2e266
antoxarbl6ak <antonmilonow3@gmail.com>
2025-04-04 22:50:06 +0300

    старые изменения парсера

--- scripts/parser.py
+++ scripts/parser.py
@@ -27,26 +27,26 @@ search.click()
 WebDriverWait(driver, 20).until(presence_of_element_located((By.ID, "resultListCommandsForm:perPage:input")))
 select_num_elements = Select(driver.find_element(By.ID, "resultListCommandsForm:perPage:input"))
 select_num_elements.select_by_value("200")
-out = open(r"../data/french-patents-temp.csv", 'w', newline='', encoding="utf-8-sig")
+out = open(r"../data/french-patents.csv", 'w', newline='', encoding="utf-8-sig")
 dictwriter = csv.DictWriter(out, ['publication number', 'name', 'date of publication', 'ipc',
                                    'application number', 'applicant', 'inventor', 'patent'])
 dictwriter.writeheader()
 for i in range(25):
     WebDriverWait(driver, 20).until(
-        presence_of_element_located((By.CSS_SELECTOR, '[id="resultListForm:resultTable:199:patentResult"]'))
+        presence_of_element_located((By.ID, "resultListForm:resultTable:199:patentResult"))
     )
     results = []
     table = driver.find_element(By.ID, "resultListForm:resultTable_data")
     for cell in table.find_elements(By.TAG_NAME, "tr"):
         pubnum = cell.find_element(By.TAG_NAME, 'a').text
-        name = cell.find_element(By.CSS_SELECTOR, '[class="ps-patent-result--title--title content--text-wrap"]').text
-        date = cell.find_element(By.CSS_SELECTOR, '[class="ps-patent-result--title--ctr-pubdate"]').text
-        ipcspan = cell.find_element(By.CSS_SELECTOR, '[class="ps-field ps-field--is-layout--inline "]')
+        name = cell.find_element(By.CSS_SELECTOR, '.ps-patent-result--title--title.content--text-wrap').text
+        date = cell.find_element(By.CSS_SELECTOR, '.ps-patent-result--title--ctr-pubdate').text
+        ipcspan = cell.find_element(By.CSS_SELECTOR, '.ps-field.ps-field--is-layout--inline')
         ipc = ipcspan.find_elements(By.TAG_NAME, 'span')[1].text
-        application = cell.find_element(By.CSS_SELECTOR, '[class="ps-field--value notranslate"]').text
-        applicant = cell.find_element(By.CSS_SELECTOR, '[class="ps-field--value ps-patent-result--applicant notranslate"]').text
-        inventor = cell.find_element(By.CSS_SELECTOR, '[class="ps-field--value ps-patent-result--inventor notranslate"]').text
-        patent = cell.find_element(By.CSS_SELECTOR, '[class="ui-outputpanel ui-widget ps-patent-result--abstract"]').text
+        application = cell.find_element(By.CSS_SELECTOR, '.ps-field--value.notranslate').text
+        applicant = cell.find_element(By.CSS_SELECTOR, '.ps-field--value.ps-patent-result--applicant.notranslate').text
+        inventor = cell.find_element(By.CSS_SELECTOR, '.ps-field--value.ps-patent-result--inventor.notranslate').text
+        patent = cell.find_element(By.CSS_SELECTOR, '.ui-outputpanel.ui-widget.ps-patent-result--abstract').text
         results.append({
             'publication number': pubnum,
             'name': name,
@@ -59,9 +59,9 @@ for i in range(25):
         })
     dictwriter.writerows(results)
     print(f"{i+1} завершено")
-    driver.find_element(By.CSS_SELECTOR, '[class="ui-commandlink ui-widget ps-link--has-icon js-paginator-next"]').click()
+    driver.find_element(By.CSS_SELECTOR, '.ui-commandlink.ui-widget.ps-link--has-icon.js-paginator-next').click()
     WebDriverWait(driver, 10).until(
-        text_to_be_present_in_element((By.CSS_SELECTOR, '[id="resultListForm:pageNumber"]'), f'{i+2}')
+        text_to_be_present_in_element((By.ID, "resultListForm:pageNumber"), f'{i+2}')
     )
     driver.refresh()
 
