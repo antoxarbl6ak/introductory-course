f2626057a36e4a1f387a76dca93f8a4df8bdac24
antoxarbl6ak <antonmilonow3@gmail.com>
2025-04-10 17:13:09 +0300

    добавил подбор гиперпараметров через randomized_search

--- scripts/astro-classifier-train.py
+++ scripts/astro-classifier-train.py
@@ -1,7 +1,7 @@
 import xgboost as xgb
 import joblib
 import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV, RandomizedSearchCV
+from sklearn.model_selection import train_test_split, StratifiedKFold, GridSearchCV, RandomizedSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, log_loss, make_scorer
 from sklearn.pipeline import Pipeline
@@ -17,7 +17,7 @@ Y = df["class"]
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42, stratify=Y)
 
 pipeline = Pipeline([
-    ('scaler', StandardScaler()),  # Масштабируем признаки
+    ('scaler', StandardScaler()),
     ('xgb', xgb.XGBClassifier(
         objective="multi:softmax",
         eval_metric='logloss',
@@ -33,34 +33,56 @@ pipeline = Pipeline([
     random_state=42
 )"""
 
-"""cv_scores = cross_val_score(
-    model,
-    x_train,
-    y_train,
-    cv=5,
-    scoring="accuracy",
-    n_jobs=-1
-)
-print("cv scores:", cv_scores)"""
 
-param_grid = {
-    'xgb__n_estimators': [600],       # Количество деревьев
-    'xgb__max_depth': [10],             # Глубина деревьев
-    'xgb__learning_rate': [0.1],    # Скорость обучения
+
+"""param_grid = {
+    'learning_rate': [0.01, 0.1, 0.2],  # Оптимальный шаг обучения
+    'max_depth': [3, 5, 7],              # Баланс сложности
+    'min_child_weight': [1, 3, 5],       # Контроль переобучения
+    'gamma': [0, 0.1, 0.2],             # Минимальное улучшение
+    'subsample': [0.6, 0.8, 1.0],            # Доля данных для дерева
+    'colsample_bytree': [0.8, 1.0],     # Доля признаков
+    'reg_alpha': [0, 0.1, 0.5],         # L1-регуляризация
+    'reg_lambda': [0.5, 1, 1.5],        # L2-регуляризация
+    'n_estimators': [100, 200, 300, 600]          # Число деревьев
 }
 grid_search = GridSearchCV(
-    estimator=pipeline,
+    estimator=pipeline.,
     param_grid=param_grid,
-    scoring="accuracy",  
-    cv=3,                 
+    scoring="roc_auc",  
+    cv=StratifiedKFold(5, shuffle=True, random_state=42),                 
     n_jobs=-1,    
 )
 grid_search.fit(x_train, y_train)
 print("best params:", grid_search.best_params_)
-print("best score:", grid_search.best_score_)
+print("best score:", grid_search.best_score_)"""
+
+param_dist = {
+    'xgb__learning_rate': uniform(0.01, 0.3),  
+    'xgb__max_depth': randint(3, 10),   
+    'xgb__min_child_weight': randint(1, 10),   
+    'xgb__gamma': uniform(0, 0.5),       
+    'xgb__subsample': uniform(0.6, 0.4),      
+    'xgb__colsample_bytree': uniform(0.6, 0.4), 
+    'xgb__reg_alpha': uniform(0, 1),      
+    'xgb__reg_lambda': uniform(0, 1),       
+    'xgb__n_estimators': randint(100, 800)      
+}
+
+random_search = RandomizedSearchCV(
+    estimator=pipeline,
+    param_distributions=param_dist,
+    n_iter=300,
+    scoring='roc_auc', 
+    cv=StratifiedKFold(n_splits=5, shuffle=True, random_state=42),
+    n_jobs=-1,
+    random_state=42
+)
+
+random_search.fit(x_train, y_train)
 
 
-best_model = grid_search.best_estimator_
+best_model = random_search.best_estimator_
 best_model.fit(x_train, y_train)
 
 y_pred = best_model.predict(x_test)
